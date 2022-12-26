0412752e9977b6399d8f1f10f7433ce8c919d346
aegar <andreybelyyw@gmail.com>
2022-12-26 15:20:46 -0600

    Lesson for bubble sort

---
+++ Lesson_bubble_sort.py
@@ -0,0 +1,14 @@
+test_l = [9, 56, 14, 0, 1, 129, 41]
+
+def bubble(test_list):
+    last_item = len(test_list) - 1
+    for t in range(0, last_item):
+        for u in range(0, last_item):
+            if test_list[u] > test_list[u+1]:
+                test_list[u], test_list[u+1] = test_list[u+1], test_list[u]
+
+    return test_list
+
+bubble(test_l)
+new_l = bubble(test_l).copy()
+print(new_l)
