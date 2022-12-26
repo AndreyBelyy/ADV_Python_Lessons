687afc3c932f1e57f57482ff74f0d36e889bee3f
aegar <andreybelyyw@gmail.com>
2022-12-26 15:33:59 -0600

    Lesson for bubble sort

--- Lesson_bubble_sort.py
+++ Lesson_bubble_sort.py
@@ -3,7 +3,7 @@ test_l = [9, 56, 14, 0, 1, 129, 41]
 def bubble(test_list):
     last_item = len(test_list) - 1
     for t in range(0, last_item):
-        for u in range(0, last_item):
+        for u in range(0, last_item-t):
             if test_list[u] > test_list[u+1]:
                 test_list[u], test_list[u+1] = test_list[u+1], test_list[u] # swap current index value with next one
 
